a5514e30999bb95a44f43fd21944f692635e7baf
jamth4 <105566351+jamth4@users.noreply.github.com>
2024-06-02 16:28:48 -0400

    code refactoring and converting to functions

--- centreon_hostgroup.py
+++ centreon_hostgroup.py
@@ -1,29 +1,51 @@
 # Import modules
 import requests
-from dotenv import load_dotenv, find_dotenv
+from dotenv import load_dotenv, find_dotenv, set_key
 import os
 import json
 
+csv_path = '<path to file>'
 dotenv_file = find_dotenv()
 load_dotenv(dotenv_file)
 
+username = os.getenv("CENTREON_USER")
+password = os.getenv("CENTREON_PASSWORD")
 centreon_url = os.getenv('CENTREON_URL')
 api_token = os.getenv('CENTREON_API')
 
-base_url = f"https://{centreon_url}/centreon/api/index.php?"
+def cent_api (domain = centreon_url, username = username, password = password):
+    '''Function to create api key if api authentication fails'''
+    base_url = f"{domain}action=authenticate"
+    todo = {"username":f"{username}", "password": f"{password}"}
+    response = requests.post(base_url, data=todo, verify=False)
 
-clapi_url = "action=action&object=centreon_clapi"
+    cent_token = response.json()
 
-headers = {"Content-Type": "application/json", "centreon-auth-token": f"{api_token}"}
+    #print(cent_token)
 
-body = {
-    "action": "show",
-    "object": "HG"
-}
+    cent_key = cent_token['authToken']
 
-response = requests.post(base_url+clapi_url, json=body, headers=headers, verify=False)
+    set_key(dotenv_file, 'CENTREON_API', cent_key) 
 
-print(response.json())
 
-with open('centreongroups.json', 'w') as f:
-    json.dump(response.json(), f, ensure_ascii=False, indent=4)
+def cent_list_hg(domain = centreon_url, cent_api=api_token):
+    base_url = f"{domain}action=action&object=centreon_clapi"
+    headers = {"Content-Type": "application/json", "centreon-auth-token": f"{cent_api}"}
+
+    body = {
+        "action": "show",
+        "object": "HG"
+    }
+    response = requests.post(base_url+clapi_url, json=body, headers=headers, verify=False)
+
+    print(response.json())
+
+    with open('centreongroups.json', 'w') as f:
+        json.dump(response.json(), f, ensure_ascii=False, indent=4)
+
+try:
+    cent_list_hg()
+
+except:
+    cent_api()
+    cent_list_hg()
