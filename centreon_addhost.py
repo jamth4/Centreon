6b7cd90e415f106bc994909dcfb0b71374feb59a
jamth4 <105566351+jamth4@users.noreply.github.com>
2024-06-02 08:13:38 -0400

    code refactoring and converting to functions

--- centreon_addhost.py
+++ centreon_addhost.py
@@ -1,26 +1,51 @@
 # Import modules
 import requests
-from dotenv import load_dotenv, find_dotenv
+from dotenv import load_dotenv, find_dotenv, set_key
 import os
+
+# Set variables and pull account info
 
 dotenv_file = find_dotenv()
 load_dotenv(dotenv_file)
 
+username = os.getenv("CENTREON_USER")
+password = os.getenv("CENTREON_PASSWORD")
 centreon_url = os.getenv('CENTREON_URL')
 api_token = os.getenv('CENTREON_API')
 
-base_url = f"https://{centreon_url}/centreon/api/index.php?"
+def cent_api (domain = centreon_url, username = username, password = password):
+    '''Function to create api key if api authentication fails'''
+    base_url = f"{domain}action=authenticate"
+    todo = {"username":f"{username}", "password": f"{password}"}
+    response = requests.post(base_url, data=todo, verify=False)
 
-clapi_url = "action=action&object=centreon_clapi"
+    cent_token = response.json()
 
-headers = {"Content-Type": "application/json", "centreon-auth-token": f"{api_token}"}
+    #print(cent_token)
 
-body = {
-    "action": "add",
-    "object": "host",
-    "values": "name;alias;ipaddress;hosttemplate;poller;hostgroup"
-}
+    cent_key = cent_token['authToken']
 
-response = requests.post(base_url+clapi_url, json=body, headers=headers, verify=False)
+    set_key(dotenv_file, 'CENTREON_API', cent_key) 
 
-print(response.json())
+def add_host(values, domain=centreon_url,token=api_token):
+    '''
+    Function to manually add a host, values need to take the form:
+    "name;alias;ipaddress;hosttemplate;poller;hostgroup"
+    usage:
+    add_host(values="pc1;pc1;10.10.10.10;OS-Linux-SSH-custom;central;Linux_Servers")
+    '''
+    clapi_url = f"{domain}action=action&object=centreon_clapi"
+    headers = {"Content-Type": "application/json", "centreon-auth-token": f"{token}"}
+    body = {
+        "action": "add",
+        "object": "host",
+        "values": values
+    }
+    response = requests.post(clapi_url, json=body, headers=headers, verify=False)
+    print(response.json())
+
+try:
+    add_host(values="pc1;pc1;10.10.10.10;OS-Linux-SSH-custom;central;Linux_Servers")
+except:
+    cent_api()
+    add_host(values="pc1;pc1;10.10.10.10;OS-Linux-SSH-custom;central;Linux_Servers")
